3268ab0150ee14739222f327cd9b582d4389fca6
m00k <christian.bican@gmail.com>
2023-12-30 17:18:58 +0100

    configure bash aliases

--- home.nix
+++ home.nix
@@ -8,10 +8,22 @@ in
     (import "${home-manager}/nixos")
   ];
 
-  home-manager.users.m00k = {
+   home-manager.users.m00k = {
     /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "23.11";
+    home = {
+      stateVersion = "23.11";
+    };
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+
+    programs.bash = {
+      enable = true;
+      shellAliases = {
+        la = "ls -lha";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../..";
+      };
+    };
 
     # GIT
     programs.git = {
