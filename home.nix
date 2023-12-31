757a61f7601b06c9f58bbaa435b81c8b804674f7
m00k <christian.bican@gmail.com>
2023-12-31 10:45:37 +0100

    configure gsettings (wip)

--- home.nix
+++ home.nix
@@ -8,18 +8,27 @@ in
 		(import "${home-manager}/nixos")
 	];
 
-	home-manager.users.m00k = {
+	home-manager.users.m00k = { lib, ... }: {
 		# HOME   
 		home = {
 			stateVersion = "23.11";
 		};
 
-    #DCONF
+    # DCONF
+    # https://nixos.org/manual/nixos/stable/#sec-gnome-gsettings-overrides
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-dconf.settings
+    # https://determinate.systems/posts/declarative-gnome-configuration-with-nixos
     dconf = {
       enable = true;
       settings = {
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
+      	"org/gnome/desktop/interface" = {
+      		color-scheme = "prefer-dark";
+        };
+        "org/gnome/desktop/session" = {
+					idle-delay = lib.hm.gvariant.mkUint32 1800;
+        };
+        "org/gnome/settings/daemon/plugins/power" = {
+					sleep-inactive-ac-timeout = lib.hm.gvariant.mkUint32 1800;
         };
       };
     };
