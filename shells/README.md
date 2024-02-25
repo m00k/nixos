f305a0fa0305b9359493730100af25ea3d49e9d3
m00k <christian.bican@gmail.com>
2024-02-25 09:07:03 +0100

    use specific node version in shell

---
+++ shells/README.md
@@ -0,0 +1,7 @@
+# Shells
+
+```bash
+cd ~/workspace/projects/private/my-project
+nix-shell ~/workspace/nixos/shells/npm.my-project.nix
+exit
+```
--- shells/npm.mg.nix
+++ shells/npm.mg.nix
@@ -5,12 +5,13 @@
 
  pkgs.mkShell {
    packages = with pkgs; [
-     nodejs
+     nodejs_20
    ];
 
    MG_NPM_GITHUB_READONLY_TOKEN = import ../.secrets/git.token.mg.npm.nix;
 
   shellHook = ''
     git status
+    node --version
   '';
  }
