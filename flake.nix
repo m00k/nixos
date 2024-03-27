c65bbc9bc9589a8a62e6d4fd8fd63206377b5f82
m00k <christian.bican@gmail.com>
2024-03-27 14:56:09 +0100

    de-duplicate building nixos configurations

--- flake.nix
+++ flake.nix
@@ -1,44 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; # TODO: "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # TODO: "github:NixOS/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-23.11"; # TODO: "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  # TODO:
-  # inputs = {
-  #   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  #   nixos-hardware.url = "github:NixOS/nixos-hardware";
-  #   home-manager = {
-  #     url = "github:nix-community/home-manager";
-  #     inputs.nixpkgs.follows = "nixpkgs";
-  #   };
-  # };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... } @ inputs:
     let
-      myConfigs =
-        rec {
-          base = {
-            hostName = "nixos";
-            userName = "m00k";
-            userEmail = "christian.bican@gmail.com";
-            system = "x86_64-linux";
-          };
-          f13 = base // {
-            hostName = "f13";
-          };
-          plexn = base // {
-            userName = "media";
-            hostName = "plexn";
-          };
+      myBaseConfig =
+        {
+          hostName = "nixos";
+          userName = "m00k";
+          userEmail = "christian.bican@gmail.com";
+          system = "x86_64-linux";
         };
-    in
-    {
-      nixosConfigurations =
-        ((myConfig: {
+      # TODO: extract to lib
+      myMkHome = myConfig:
+        {
           ${myConfig.hostName} =
             nixpkgs.lib.nixosSystem
               {
@@ -51,9 +32,6 @@
                   ./system/configuration.nix
                   ./hosts/${hostName}/configuration.ext.nix
                   ./hosts/${hostName}/hardware-configuration.nix
-                  ./hosts/${hostName}/hardware-configuration.ext.nix
-                  # https://github.com/NixOS/nixos-hardware/blob/master/README.md#using-nix-flakes-support
-                  nixos-hardware.nixosModules.framework-13-7040-amd
                   # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/start-using-home-manager
                   home-manager.nixosModules.home-manager
                   {
@@ -67,33 +45,19 @@
                   }
                 ];
               };
-        }) myConfigs.f13)
-
+        };
+    in
+    {
+      nixosConfigurations =
+        myMkHome
+          (myBaseConfig // {
+            hostName = "f13";
+          })
         //
-
-        ((myConfig: {
-          ${myConfig.hostName} = nixpkgs.lib.nixosSystem {
-            inherit (myConfig) system;
-            specialArgs = {
-              inherit inputs;
-              inherit myConfig;
-            };
-            modules = with myConfig; [
-              ./system/configuration.nix
-              ./hosts/${hostName}/configuration.ext.nix
-              ./hosts/${hostName}/hardware-configuration.nix
-              home-manager.nixosModules.home-manager # TODO: de-duplicate
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.${userName} = import ./users/${userName}.nix;
-                home-manager.extraSpecialArgs = {
-                  inherit inputs;
-                  inherit myConfig;
-                };
-              }
-            ];
-          };
-        }) myConfigs.plexn);
+        myMkHome
+          (myBaseConfig // {
+            userName = "media";
+            hostName = "plexn";
+          });
     };
 }
